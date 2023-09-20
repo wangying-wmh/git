ac7930cf4cc2d1990ac7b05ee4ab7bdec7a3090d
wangying <3237329857@qq.com>
2023-09-20 15:58:31 +0800

    1111

---
+++ src/main/java/main.java
@@ -0,0 +1,15 @@
+public class main {
+    public static void main(String[] args) {
+        System.out.println("这是一个git测试类");
+        System.out.println("这是一个git测试类");
+        System.out.println("这是一个git测试类");
+        System.out.println("这是一个git测试类");
+        System.out.println("这是一个git测试类");
+        System.out.println("这是一个git测试类");
+        System.out.println("这是一个git测试类");
+        System.out.println("这是一个git测试类");
+        System.out.println("这是一个git测试类");
+        System.out.println("这是一个git测试类");
+        System.out.println("这是一个git测试类");
+    }
+}
